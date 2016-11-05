09313f83d03123a75c6c6bdc766182a65e2568c6
abdullahselek <abdullahselek@gmail.com>
2016-11-05 23:43:05 +0100

    - create workspace - add pod dependencies

---
+++ DragDropUI/Pods/Target Support Files/Pods-DragDropUI/Pods-DragDropUI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DragDropUI : NSObject
+@end
+@implementation PodsDummy_Pods_DragDropUI
+@end
---
+++ DragDropUI/Pods/Target Support Files/Pods-DragDropUITests/Pods-DragDropUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DragDropUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DragDropUITests
+@end
