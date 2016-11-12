94dc4ceca3d649fda6f0c9cd112e99def23f2b48
abdullahselek <abdullahselek@gmail.com>
2016-11-12 13:51:14 +0100

    - update Nimble to 5.1.1

--- Pods/Target Support Files/Nimble/Nimble-umbrella.h
+++ Pods/Target Support Files/Nimble/Nimble-umbrella.h
@@ -2,6 +2,9 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#import "CwlCatchException.h"
+#import "CwlCatchBadInstruction.h"
+#import "mach_excServer.h"
 #import "Nimble.h"
 #import "DSL.h"
 #import "NMBExceptionCapture.h"
