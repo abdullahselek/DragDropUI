45e1c370821edbf63ff169d00d5eb6d4c54ca4fe
abdullahselek <abdullahselek@gmail.com>
2016-12-01 21:08:27 +0100

    - new tests

--- DragDropUITests/DDViewTests.swift
+++ DragDropUITests/DDViewTests.swift
@@ -33,96 +33,30 @@ import Nimble
 class DDViewTests: QuickSpec {
     
     override func spec() {
-        /***
         describe("DragDropView", {
-            context("view init", {
-                it("withFrame should initiate successfully") {
-                    let view = DDView(frame: CGRect(x: 0.0, y: 0.0, width: 200.0, height: 40.0))
+            context("init with frame", {
+                it("should initiate successfully", closure: {
+                    let view = DDView(frame:  CGRect(x: 0.0, y: 0.0, width: 200.0, height: 40.0))
                     expect(view).notTo(beNil())
-                    expect(view.gestureRecognizers).to(haveCount(1))
-                }
+                })
             })
-            context("view wasDragged", {
-                it("and delegate not nil should inform the view controller") {
-                    let viewController = TestViewControler()
-                    let view = MockDDView(frame: CGRect(x: 0.0, y: 0.0, width: 200.0, height: 40.0))
-                    expect(view).notTo(beNil())
-                    view.delegate = viewController
-                    expect(view.delegate).notTo(beNil())
-                    let gestureRecognizer = MockPanGestureRecognizer(target: view, action: #selector(DDView.handlePan(_:)))
-                    view.addGestureRecognizer(gestureRecognizer)
+            context("didMoveToSuperview", {
+                it("should have gesture recognizers if superview not nil", closure: {
+                    let viewController = UIViewController()
+                    let view = DDView(frame:  CGRect(x: 0.0, y: 0.0, width: 200.0, height: 40.0))
                     viewController.view.addSubview(view)
-                    view.handlePan(gestureRecognizer)
-                    expect(viewController.wasDragged).to(beTrue())
-                }
+                    let _ = viewController.view
+                    expect(view.gestureRecognizers).to(haveCount(2))
+                })
             })
-            context("view wasDragged", {
-                it("and delegate nil should not inform the view controller") {
-                    let viewController = TestViewControler()
-                    let view = MockDDView(frame: CGRect(x: 0.0, y: 0.0, width: 200.0, height: 40.0))
-                    expect(view).notTo(beNil())
-                    expect(view.delegate).to(beNil())
-                    let gestureRecognizer = UIPanGestureRecognizer(target: view, action: #selector(DDView.handlePan(_:)))
-                    view.addGestureRecognizer(gestureRecognizer)
-                    viewController.view.addSubview(view)
-                    view.handlePan(gestureRecognizer)
-                    expect(viewController.wasDragged).to(beFalse())
-                }
-            })
-            context("view wasDropped", {
-                it("and delegate not nil should inform the view controller") {
-                    let viewController = TestViewControler()
-                    let view = MockDDView(frame: CGRect(x: 0.0, y: 0.0, width: 200.0, height: 40.0))
-                    expect(view).notTo(beNil())
-                    view.delegate = viewController
-                    expect(view.delegate).notTo(beNil())
-                    let gestureRecognizer = UIPanGestureRecognizer(target: view, action: #selector(DDView.handlePan(_:)))
-                    view.addGestureRecognizer(gestureRecognizer)
-                    viewController.view.addSubview(view)
-                    view.handlePan(gestureRecognizer)
-                    expect(viewController.wasDropped).to(beTrue())
-                }
-            })
-            context("view wasDropped", {
-                it("and delegate not nil should not inform the view controller") {
-                    let viewController = TestViewControler()
-                    let view = MockDDView(frame: CGRect(x: 0.0, y: 0.0, width: 200.0, height: 40.0))
-                    expect(view).notTo(beNil())
-                    expect(view.delegate).to(beNil())
-                    let gestureRecognizer = UIPanGestureRecognizer(target: view, action: #selector(DDView.handlePan(_:)))
-                    view.addGestureRecognizer(gestureRecognizer)
-                    viewController.view.addSubview(view)
-                    view.handlePan(gestureRecognizer)
-                    expect(viewController.wasDropped).to(beFalse())
-                }
+            context("didMoveToSuperview", {
+                it("should not have gesture recognizers if superview nil", closure: {
+                    let view = DDView(frame:  CGRect(x: 0.0, y: 0.0, width: 200.0, height: 40.0))
+                    view.didMoveToSuperview()
+                    expect(view.gestureRecognizers).to(beNil())
+                })
             })
         })
-        ***/
     }
     
 }
-
-/***
-private class MockDDView: DDView {
-    
-    @objc override func handlePan(_ gestureRecognizer: UIPanGestureRecognizer) {
-        super.handlePan(gestureRecognizer)
-    }
-    
-}
-
-private class TestViewControler: UIViewController, DDViewDelegate {
-    
-    public var wasDragged: Bool! = false
-    public var wasDropped: Bool! = false
-    
-    public func viewWasDropped(view: UIView, droppedPoint: CGPoint) {
-        wasDropped = true
-    }
-    
-    public func viewWasDragged(view: UIView, draggedPoint: CGPoint) {
-        wasDragged = true
-    }
-    
-}
-***/
